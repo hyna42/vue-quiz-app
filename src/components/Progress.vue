017575f07b7ec3a95fd802d6bd69dfc9214f3e61
hyna42 <bylo@duck.com>
2026-02-11 22:17:51 +0100

    feat: add Progress component with template and styles

---
+++ src/components/Progress.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps<{
+  max: number;
+  value: number;
+}>();
+</script>
+
+<template>
+  <div class="progress">
+    <progress :value="value" :max="max" />
+    <span>{{ Math.round((value / max) * 100) }}%</span>
+  </div>
+</template>
+
+<style scoped>
+.progress {
+  display: flex;
+  align-items: center;
+  
+}
+progress{
+    margin-bottom: 0;
+    margin-right: 1rem;
+}
+</style>
