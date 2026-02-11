146be02f2cef32692b12306b3058fe2461f66741
hyna42 <bylo@duck.com>
2026-02-11 20:50:19 +0100

    feat :  handle loading and error states

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,40 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useFetchQuestions } from "./composables/Api";
+import Quiz from "./components/Quiz.vue";
+
+const { error, loading, questions, fetchQuestions } = useFetchQuestions();
+
+onMounted(async () => {
+  const url = "https://opentdb.com/api.php?amount=10";
+  await fetchQuestions(url);
+
+  console.log("error", error.value);
+  console.log("loading", loading.value);
+  console.log("questions", questions.value);
+});
+</script>
 
 <template>
-  <p>Hello</p>
+  <main>
+    <div v-if="error" class="quiz-message">
+      <div class="alert alert-error">❌ {{ error }}</div>
+    </div>
+    <div v-else>
+      <div v-if="loading" class="quiz-message" :aria-busy="true">
+        <p>Chargement des questions ...</p>
+      </div>
+      <Quiz v-else :questions="questions" />
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.quiz-message {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 50vh;
+  width: 100%;
+}
+</style>
