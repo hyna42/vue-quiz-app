fcd61f9452930da51b399f6742b8b12ad16694ad
hyna42 <bylo@duck.com>
2026-02-12 23:13:59 +0100

    feat: implement Quiz component

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,23 +1,64 @@
 <script setup lang="ts">
 import type { TriviaQuestion } from "@/composables/Api";
+import Progress from "./Progress.vue";
+import { computed, ref } from "vue";
+import Question from "./Question.vue";
 
+//props
 const props = defineProps<{
   questions: TriviaQuestion[];
 }>();
+
+//variables
+const step = ref(0);
+const end = ref(false);
+
+//all responses
+const answers = ref<(string | null)[]>(props.questions.map(() => null));
+
+//compute
+const currentQuestion = computed(() => props.questions[step.value] || null);
+
+//fonctions
+const addAnswer = (answer: string) => {
+  //incremente answers
+  answers.value[step.value] = answer;
+  //si on est en dernier : afficher un texte
+  if (step.value === props.questions.length - 1) {
+    end.value = true;
+    console.log("🎉 Quiz terminé !");
+  } else {
+    //sinon incrémente
+    step.value++;
+    console.log(`Question ${step.value + 1}/${props.questions.length}`);
+  }
+};
 </script>
 
 <template>
   <div class="quiz">
     <!-- title -->
     <h1>General Knowledge Quiz</h1>
-    <pre>{{ questions }}</pre>
-    <!-- progress bar -->
-
-    <!-- question -->
-
-    <!-- answers : 1 choice == radio -->
-
-    <!-- button : next question -->
+    <div v-if="end">
+      <p>Evaluation ...</p>
+    </div>
+    <div v-else>
+      <!-- progress bar -->
+      <Progress :max="questions.length" :value="step + 1" />
+      <!-- question -->
+      <Question
+        v-if="currentQuestion"
+        :question="currentQuestion"
+        @answer="addAnswer"
+      />
+    </div>
+    <hr />
+    <pre>
+      {{ currentQuestion }}
+    </pre>
+    <pre>
+      {{ answers }}
+    </pre>
   </div>
 </template>
 
@@ -31,4 +72,3 @@ h1 {
   padding: 0 1rem;
 }
 </style>
-
