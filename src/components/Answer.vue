5bb24a203885e8a090cd1a3700cd218a6f1ee089
hyna42 <bylo@duck.com>
2026-02-14 19:10:02 +0100

    refactor: create Answer component

---
+++ src/components/Answer.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+    shuffledAnswers: string[];
+    disabled:boolean
+}>();
+
+const model = defineModel<(string|null)>();
+</script>
+
+<template>
+  <label
+    v-for="(answer, index) in shuffledAnswers"
+    :key="index"
+    :for="`answer${index}`"
+  >
+    <input
+      :id="`answer${index}`"
+      type="radio"
+      name="answer"
+      :value="answer"
+      v-model="model"
+      :disabled="disabled"
+    />
+    {{ answer }}
+  </label>
+</template>
